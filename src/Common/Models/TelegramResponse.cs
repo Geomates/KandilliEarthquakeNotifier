e59793652a7a21f8c98687ae8b1810c36e45fc2d
Mete Ercan Pakdil <mete.pakdil@mottmac.com>
2020-01-06 12:21:22 +0000

    add missing json attributes

--- src/Common/Models/TelegramResponse.cs
+++ src/Common/Models/TelegramResponse.cs
@@ -1,8 +1,13 @@
-﻿namespace Common.Models
+﻿using Newtonsoft.Json;
+
+namespace Common.Models
 {
     public class TelegramResponse
     {
+        [JsonProperty("error_code")]
         public int ErrorCode { get; }
+
+        [JsonProperty("description")]
         public string Description { get; }
     }
 }
