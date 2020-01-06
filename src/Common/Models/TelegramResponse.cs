2f92f167e339cfe646be9241f447639d9db885aa
Mete Ercan Pakdil <mete.pakdil@mottmac.com>
2020-01-06 12:34:51 +0000

    fix model

--- src/Common/Models/TelegramResponse.cs
+++ src/Common/Models/TelegramResponse.cs
@@ -5,9 +5,9 @@ namespace Common.Models
     public class TelegramResponse
     {
         [JsonProperty("error_code")]
-        public int ErrorCode { get; }
+        public int ErrorCode { get; set; }
 
         [JsonProperty("description")]
-        public string Description { get; }
+        public string Description { get; set; }
     }
 }
